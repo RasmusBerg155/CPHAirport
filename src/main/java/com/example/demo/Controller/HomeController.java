8eb330c03a0fa7e811ef680569defdbbb321c9c0
kasper <kasperkbf@hotmail.com>
2021-03-15 14:09:07 +0100

    Initial Controller/HTML

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "index";
     }
