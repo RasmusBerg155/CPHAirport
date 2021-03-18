3043b41ae018f23361c25d21b45ee2772e2d9cd4
Jakob975 <58294235+Jakob975@users.noreply.github.com>
2021-03-18 13:40:20 +0100

    Model classer lavet.

---
+++ src/main/java/com/example/demo/Model/Working.java
@@ -0,0 +1,27 @@
+package com.example.demo.Model;
+
+public class Working {
+
+    int workingTime;
+    int walkingTime;
+
+    public Working() {
+
+    }
+
+    public int getWorkingTime() {
+        return workingTime;
+    }
+
+    public void setWorkingTime(int workingTime) {
+        this.workingTime = workingTime;
+    }
+
+    public int getWalkingTime() {
+        return walkingTime;
+    }
+
+    public void setWalkingTime(int walkingTime) {
+        this.walkingTime = walkingTime;
+    }
+}
