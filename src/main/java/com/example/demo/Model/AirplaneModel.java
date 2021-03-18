bb9e1af62d401e4b95e15c43ef8f2a53f8d90a40
Jakob975 <58294235+Jakob975@users.noreply.github.com>
2021-03-18 11:42:57 +0100

    Model classer lavet.

--- src/main/java/com/example/demo/Model/AirplaneModel.java
+++ src/main/java/com/example/demo/Model/AirplaneModel.java
@@ -1,4 +1,79 @@
 package com.example.demo.Model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class AirplaneModel {
+    int id;
+    int airPlaneSize;
+    LocalDate arivalDate;
+    LocalTime arivalTime;
+    LocalDate departureDate;
+    LocalTime departureTime;
+    Boolean phase;
+
+    public AirplaneModel(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAirPlaneSize() {
+        return airPlaneSize;
+    }
+
+    public void setAirPlaneSize(int airPlaneSize) {
+        this.airPlaneSize = airPlaneSize;
+    }
+
+    public LocalDate getArivalDate() {
+        return arivalDate;
+    }
+
+    public void setArivalDate(LocalDate arivalDate) {
+        this.arivalDate = arivalDate;
+    }
+
+    public LocalTime getArivalTime() {
+        return arivalTime;
+    }
+
+    public void setArivalTime(LocalTime arivalTime) {
+        this.arivalTime = arivalTime;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Boolean getPhase() {
+        return phase;
+    }
+
+    public void setPhase(Boolean phase) {
+        this.phase = phase;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+
+
+
+
+
+    }
 }
---
+++ src/main/java/com/example/demo/Model/Finger.java
@@ -0,0 +1,36 @@
+package com.example.demo.Model;
+
+public class Finger {
+
+    public Finger() {
+
+    }
+    int fingerId;
+    int gateId;
+    int walkingTime;
+
+    public int getFingerId() {
+        return fingerId;
+    }
+
+    public void setFingerId(int fingerId) {
+        this.fingerId = fingerId;
+    }
+
+    public int getGateId() {
+        return gateId;
+    }
+
+    public void setGateId(int gateId) {
+        this.gateId = gateId;
+    }
+
+    public int getWalkingTime() {
+        return walkingTime;
+    }
+
+    public void setWalkingTime(int walkingTime) {
+        this.walkingTime = walkingTime;
+    }
+
+}
